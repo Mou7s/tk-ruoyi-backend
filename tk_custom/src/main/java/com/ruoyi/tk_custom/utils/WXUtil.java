d2c3011b8bad5a3131d60f61ef94cf81245d4e40
mou7s <liuchuge7777777@gmail.com>
2026-02-07 09:12:40 +0800

    refactor: 格式化 pom.xml 和 WXUtil.java 文件，优化代码可读性

--- tk_custom/src/main/java/com/ruoyi/tk_custom/utils/WXUtil.java
+++ tk_custom/src/main/java/com/ruoyi/tk_custom/utils/WXUtil.java
@@ -35,7 +35,6 @@ import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class WXUtil {
 
     private static ExecutorService executorService = Executors.newCachedThreadPool();
@@ -55,7 +54,9 @@ public class WXUtil {
             System.out.println("老token");
             return accessToken;
         }
-        String body = HttpRequest.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + corpid + "&corpsecret=" + corpsecret).body();
+        String body = HttpRequest
+                .get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + corpid + "&corpsecret=" + corpsecret)
+                .body();
         HashMap hashMap = new Gson().fromJson(body, HashMap.class);
         accessToken = hashMap.get("access_token").toString();
         getTokenTime = new Date();
@@ -66,25 +67,29 @@ public class WXUtil {
 
     public static String getToken_TongXunLu() {
         if (getTokenTime_TongXunLu != null && (new Date().getTime() - getTokenTime_TongXunLu.getTime()) / 1000 < 7000) {
-//            System.out.println("老token");
+            // System.out.println("老token");
             return accessToken_TongXunLu;
         }
-        String body = HttpRequest.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + corpid + "&corpsecret=" + corpsecret_TongXunLu).body();
+        String body = HttpRequest.get(
+                "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + corpid + "&corpsecret=" + corpsecret_TongXunLu)
+                .body();
         HashMap hashMap = new Gson().fromJson(body, HashMap.class);
         accessToken_TongXunLu = hashMap.get("access_token").toString();
         getTokenTime_TongXunLu = new Date();
-//        System.out.println("重新获取token");
+        // System.out.println("重新获取token");
         return accessToken_TongXunLu;
-    }
+    }   
 
     public static ArrayList<JsonObject> getUserObjectListFrom_TongXunLu(int times) {
-        HttpRequest httpRequest = HttpRequest.post("https://qyapi.weixin.qq.com/cgi-bin/user/list_id?access_token=" + getToken_TongXunLu()).acceptJson();
+        HttpRequest httpRequest = HttpRequest
+                .post("https://qyapi.weixin.qq.com/cgi-bin/user/list_id?access_token=" + getToken_TongXunLu())
+                .acceptJson();
         HashMap<String, String> hp = new HashMap<>();
         if (times > 99)
             return null;
         times++;
         String body = httpRequest.send(new Gson().toJson(hp)).body();
-        if (!"ok".equals(new Gson().fromJson(body, JsonObject.class).get("errmsg").getAsString())) {//请求失败
+        if (!"ok".equals(new Gson().fromJson(body, JsonObject.class).get("errmsg").getAsString())) {// 请求失败
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
@@ -104,13 +109,14 @@ public class WXUtil {
     }
 
     public static JsonArray getDept(int times) {
-        HttpRequest httpRequest = HttpRequest.post("https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=" + getToken()).acceptJson();
+        HttpRequest httpRequest = HttpRequest
+                .post("https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=" + getToken()).acceptJson();
         HashMap<String, String> hp = new HashMap<>();
         if (times > 99)
             return null;
         times++;
         String body = httpRequest.send(new Gson().toJson(hp)).body();
-        if (!"ok".equals(new Gson().fromJson(body, JsonObject.class).get("errmsg").getAsString())) {//请求失败
+        if (!"ok".equals(new Gson().fromJson(body, JsonObject.class).get("errmsg").getAsString())) {// 请求失败
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -124,7 +130,8 @@ public class WXUtil {
     }
 
     public static String createChatRoom() {
-        HttpRequest httpRequest = HttpRequest.post("https://qyapi.weixin.qq.com/cgi-bin/appchat/create?access_token=" + getToken()).acceptJson();
+        HttpRequest httpRequest = HttpRequest
+                .post("https://qyapi.weixin.qq.com/cgi-bin/appchat/create?access_token=" + getToken()).acceptJson();
         HashMap<String, String> hp = new HashMap<>();
         return httpRequest.send("{\n" +
                 "    \"name\" : \"物品放行记录\",\n" +
@@ -135,19 +142,25 @@ public class WXUtil {
     }
 
     public static String getUserNamebyId(String id) {
-        String body = HttpRequest.get("https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=" + getToken() + "&userid=" + id).body();
+        String body = HttpRequest
+                .get("https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=" + getToken() + "&userid=" + id)
+                .body();
         String name = new Gson().fromJson(body, JsonObject.class).get("name").getAsString();
         return name;
     }
 
     public static JsonObject getUserInfoById(String id) {
-        String body = HttpRequest.get("https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=" + getToken() + "&userid=" + id).body();
+        String body = HttpRequest
+                .get("https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=" + getToken() + "&userid=" + id)
+                .body();
         JsonObject jb = new Gson().fromJson(body, JsonObject.class);
         return jb;
     }
 
     public static JsonObject getUserbyId(String id) {
-        String body = HttpRequest.get("https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=" + getToken() + "&userid=" + id).body();
+        String body = HttpRequest
+                .get("https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=" + getToken() + "&userid=" + id)
+                .body();
         JsonObject jb = new Gson().fromJson(body, JsonObject.class);
         return jb;
     }
@@ -166,7 +179,8 @@ public class WXUtil {
             return;
         }
         // .url("https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token="+getToken()+"&type=file")
-        HttpPost httpPost = new HttpPost("https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=" + getToken() + "&type=file&debug=1");
+        HttpPost httpPost = new HttpPost(
+                "https://qyapi.weixin.qq.com/cgi-bin/media/upload?access_token=" + getToken() + "&type=file&debug=1");
         httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                 + "AppleWebKit/537.36 (KHTML, like Gecko) "
                 + "Chrome/62.0.3202.89 Safari/537.36");
@@ -253,7 +267,8 @@ public class WXUtil {
     }
 
     public static String sendMsg(String id, String content) {
-        HttpRequest httpRequest = HttpRequest.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + getToken()).acceptJson();
+        HttpRequest httpRequest = HttpRequest
+                .post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + getToken()).acceptJson();
         return httpRequest.send("{\n" +
                 "   \"touser\" : \"" + id + "\",\n" +
                 "   \"toparty\" : \"\",\n" +
@@ -270,8 +285,9 @@ public class WXUtil {
                 "}").body();
     }
 
-    public static String sendMsgToChatRoom(HashMap<String, Object> hp) {//88888888
-        HttpRequest httpRequest = HttpRequest.post("https://qyapi.weixin.qq.com/cgi-bin/appchat/send?access_token=" + getToken()).acceptJson();
+    public static String sendMsgToChatRoom(HashMap<String, Object> hp) {// 88888888
+        HttpRequest httpRequest = HttpRequest
+                .post("https://qyapi.weixin.qq.com/cgi-bin/appchat/send?access_token=" + getToken()).acceptJson();
         return httpRequest.send(new Gson().toJson(hp)).body();
     }
 
@@ -286,10 +302,11 @@ public class WXUtil {
         return SensitiveWordHelper.contains(words);
     }
 
-
-    public static ArrayList<WxApprove> getAllApproveData(long starttime, long endtime, ArrayList arrs, String new_cursor,String tempId) {
-        HttpRequest httpRequest = HttpRequest.post("https://qyapi.weixin.qq.com/cgi-bin/oa/getapprovalinfo?access_token=" + getToken()).acceptJson();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
+    public static ArrayList<WxApprove> getAllApproveData(long starttime, long endtime, ArrayList arrs,
+            String new_cursor, String tempId) {
+        HttpRequest httpRequest = HttpRequest
+                .post("https://qyapi.weixin.qq.com/cgi-bin/oa/getapprovalinfo?access_token=" + getToken()).acceptJson();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String body = httpRequest.send("{\n" +
                 "    \"starttime\" : \"" + starttime + "\",\n" +
                 "    \"endtime\" : \"" + endtime + "\",\n" +
@@ -298,7 +315,7 @@ public class WXUtil {
                 "    \"filters\" : [\n" +
                 "        {\n" +
                 "            \"key\": \"template_id\",\n" +
-                "            \"value\": \""+tempId+"\"\n" +
+                "            \"value\": \"" + tempId + "\"\n" +
                 "        },\n" +
                 "        {\n" +
                 "            \"key\" : \"sp_status\",\n" +
@@ -313,10 +330,10 @@ public class WXUtil {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            getAllApproveData(starttime, endtime, arrs, new_cursor,tempId);
+            getAllApproveData(starttime, endtime, arrs, new_cursor, tempId);
         }
         if (jsonObject.has("new_next_cursor")) {
-            getAllApproveData(starttime, endtime, arrs, jsonObject.get("new_next_cursor").getAsString(),tempId);
+            getAllApproveData(starttime, endtime, arrs, jsonObject.get("new_next_cursor").getAsString(), tempId);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -325,7 +342,7 @@ public class WXUtil {
         }
         JsonArray checkindata = jsonObject.getAsJsonArray("sp_no_list");
         for (JsonElement spNo : checkindata) {
-            //todo
+            // todo
             String spnoStr = spNo.getAsString();
             IWxApproveService iWxApproveService = SpringContextUtils.getBean(IWxApproveService.class);
             WxApprove wxApproveDB = iWxApproveService.selectWxApproveBySpNo(spnoStr);
@@ -338,7 +355,9 @@ public class WXUtil {
     }
 
     public static WxApprove getAndSaveApproveDetail(String detailID) {
-        HttpRequest httpRequest = HttpRequest.post("https://qyapi.weixin.qq.com/cgi-bin/oa/getapprovaldetail?access_token=" + getToken()).acceptJson();
+        HttpRequest httpRequest = HttpRequest
+                .post("https://qyapi.weixin.qq.com/cgi-bin/oa/getapprovaldetail?access_token=" + getToken())
+                .acceptJson();
         String body = httpRequest.send("{\n" +
                 "   \"sp_no\" : \"" + detailID + "\"\n" +
                 "}").body();
@@ -360,7 +379,8 @@ public class WXUtil {
         wxApprove.setApplyTime(new Date(info.get("apply_time").getAsLong() * 1000));
         wxApprove.setApplyerUserid(info.getAsJsonObject("applyer").get("userid").getAsString());
         wxApprove.setApplyerPartyid(info.getAsJsonObject("applyer").get("partyid").getAsString());
-        wxApprove.setApplyDataContents(getApplyDataItemValue(info.getAsJsonObject("apply_data").get("contents").getAsJsonArray()));
+        wxApprove.setApplyDataContents(
+                getApplyDataItemValue(info.getAsJsonObject("apply_data").get("contents").getAsJsonArray()));
         JsonArray records = info.get("sp_record").getAsJsonArray();
         StringBuilder sb = new StringBuilder();
         for (JsonElement record : records) {
@@ -385,13 +405,16 @@ public class WXUtil {
             JsonObject jb = je.getAsJsonObject();
             String control = jb.get("control").getAsString();
             if ("Date".equals(control)) {
-                long asLong = jb.get("value").getAsJsonObject().get("date").getAsJsonObject().get("s_timestamp").getAsLong();
+                long asLong = jb.get("value").getAsJsonObject().get("date").getAsJsonObject().get("s_timestamp")
+                        .getAsLong();
                 returnJb.put(jb.get("title").getAsJsonArray().get(0).getAsJsonObject().get("text").getAsString(),
                         simpleDateFormat.format(new Date(asLong * 1000)));
             }
             if ("Selector".equals(control)) {
                 returnJb.put("needBack",
-                        jb.get("value").getAsJsonObject().get("selector").getAsJsonObject().get("options").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsJsonArray().get(0).getAsJsonObject().get("text").getAsString());
+                        jb.get("value").getAsJsonObject().get("selector").getAsJsonObject().get("options")
+                                .getAsJsonArray().get(0).getAsJsonObject().get("value").getAsJsonArray().get(0)
+                                .getAsJsonObject().get("text").getAsString());
             }
             if ("Contact".equals(control)) {
                 JsonArray arr1 = jb.get("value").getAsJsonObject().get("members").getAsJsonArray();
@@ -417,17 +440,20 @@ public class WXUtil {
                         jb.get("value").getAsJsonObject().get("text").getAsString());
             }
             if ("Attendance".equals(control)) {
-                JsonObject asJsonObject = jb.get("value").getAsJsonObject().get("attendance").getAsJsonObject().get("date_range").getAsJsonObject();
-                JsonObject slice_info = jb.get("value").getAsJsonObject().get("attendance").getAsJsonObject().get("slice_info").getAsJsonObject();
+                JsonObject asJsonObject = jb.get("value").getAsJsonObject().get("attendance").getAsJsonObject()
+                        .get("date_range").getAsJsonObject();
+                JsonObject slice_info = jb.get("value").getAsJsonObject().get("attendance").getAsJsonObject()
+                        .get("slice_info").getAsJsonObject();
 
-                String starttime=simpleDateFormat.format(new Date(asJsonObject.get("new_begin").getAsLong()*1000));
-                String endtime=simpleDateFormat.format(new Date(asJsonObject.get("new_end").getAsLong()*1000));
-                String time=""+slice_info.get("duration").getAsLong()/60/60;
+                String starttime = simpleDateFormat.format(new Date(asJsonObject.get("new_begin").getAsLong() * 1000));
+                String endtime = simpleDateFormat.format(new Date(asJsonObject.get("new_end").getAsLong() * 1000));
+                String time = "" + slice_info.get("duration").getAsLong() / 60 / 60;
                 JSONObject jsonObject = new JSONObject();
-                jsonObject.put("starttime",starttime);
-                jsonObject.put("endtime",endtime);
-                jsonObject.put("time",time);
-                returnJb.put(jb.get("title").getAsJsonArray().get(0).getAsJsonObject().get("text").getAsString(),jsonObject.toString());
+                jsonObject.put("starttime", starttime);
+                jsonObject.put("endtime", endtime);
+                jsonObject.put("time", time);
+                returnJb.put(jb.get("title").getAsJsonArray().get(0).getAsJsonObject().get("text").getAsString(),
+                        jsonObject.toString());
             }
             if ("File".equals(control)) {
                 JsonArray asJsonArray = jb.get("value").getAsJsonObject().get("files").getAsJsonArray();
@@ -465,9 +491,10 @@ public class WXUtil {
     public static String downloadNet(String media_id) throws IOException {
         int bytesum = 0;
         int byteread;
-        URL url = new URL("https://qyapi.weixin.qq.com/cgi-bin/media/get?access_token=" + getToken() + "&media_id=" + media_id + "");
-//        String path = RuoYiConfig.getProfile() + "//wx_pic";
-        String path =RuoYiConfig.getProfile()+ "\\wx_pic";
+        URL url = new URL("https://qyapi.weixin.qq.com/cgi-bin/media/get?access_token=" + getToken() + "&media_id="
+                + media_id + "");
+        // String path = RuoYiConfig.getProfile() + "//wx_pic";
+        String path = RuoYiConfig.getProfile() + "\\wx_pic";
         File file = new File(path);
         boolean canSave = true;
         if (!file.exists()) {
@@ -491,26 +518,28 @@ public class WXUtil {
         } catch (Exception e) {
             return null;
         }
-        zipFile(path,fileName);
+        zipFile(path, fileName);
         return fileName;
     }
 
     public static void main(String[] args) {
         System.out.println(new Date().getTime());
     }
-    public static void zipFile(String path,String fileName){
-        File file = new File(path, fileName+".png");
-        long size=file.length()/1024;
-        float scale=0.3f;
-        float quality=1f;
-        String zipDir=path;
-        if(!path.contains("pic_zip"));
-            zipDir=path.replace("wx_pic","pic_zip");
+
+    public static void zipFile(String path, String fileName) {
+        File file = new File(path, fileName + ".png");
+        long size = file.length() / 1024;
+        float scale = 0.3f;
+        float quality = 1f;
+        String zipDir = path;
+        if (!path.contains("pic_zip"))
+            ;
+        zipDir = path.replace("wx_pic", "pic_zip");
         File file1 = new File(zipDir);
         if (!file1.exists())
             file1.mkdirs();
-        String srcPath=path + "\\" + fileName + ".png";
-        String zipPath=zipDir+ "\\" + fileName + ".png";
+        String srcPath = path + "\\" + fileName + ".png";
+        String zipPath = zipDir + "\\" + fileName + ".png";
         try {
             Thumbnails.of(srcPath)
                     .scale(scale)
@@ -521,17 +550,18 @@ public class WXUtil {
         }
         System.out.println("压缩1次");
         File zipFile = new File(zipPath);
-        if((zipFile.length()/1024)>1000)
-            zipFile(zipDir,fileName);
+        if ((zipFile.length() / 1024) > 1000)
+            zipFile(zipDir, fileName);
     }
-    public static void tarFile(String srcPath,String destPath){
-        float scale=1f;
-        float quality=1f;
+
+    public static void tarFile(String srcPath, String destPath) {
+        float scale = 1f;
+        float quality = 1f;
         File file = new File(destPath);
-        if(file.length()/1024>100)
-            scale=0.3f;
-        if(!file.exists()) {
-            if(!file.getParentFile().exists())
+        if (file.length() / 1024 > 100)
+            scale = 0.3f;
+        if (!file.exists()) {
+            if (!file.getParentFile().exists())
                 file.getParentFile().mkdirs();
             try {
                 boolean newFile = file.createNewFile();
@@ -548,7 +578,7 @@ public class WXUtil {
             return;
         }
         File destFile = new File(destPath);
-        if((destFile.length()/1024)>100)
-            tarFile(destPath,destPath);
+        if ((destFile.length() / 1024) > 100)
+            tarFile(destPath, destPath);
     }
 }
